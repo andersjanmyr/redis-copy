bd25df575f035d0e763cb21b992fd414d89ad0bb
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-05-27 11:00:28 +0200

    Added force and help options

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -16,11 +18,18 @@ func NewClient(url string) *redis.Client {
 	return client
 }
 
-func copyData(keys []string, from *redis.Client, to *redis.Client) {
+func copyData(keys []string, from *redis.Client, to *redis.Client, force *bool) {
 	for _, key := range keys {
 		dump, err := from.Dump(key).Result()
 		if err != nil {
 			log.Fatal(err)
+		}
+		if *force && to.Exists(key).Val() {
+			log.Println("Deleting existing key due to --force:", key)
+			_, err = to.Del(key).Result()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
 		}
 		_, err = to.Restore(key, 0, dump).Result()
 		if err != nil {
@@ -30,9 +39,20 @@ func copyData(keys []string, from *redis.Client, to *redis.Client) {
 }
 
 func main() {
-	log.Printf("Copying data from: '%s', to: '%s'\n", os.Args[1], os.Args[2])
-	from := NewClient(os.Args[1])
-	to := NewClient(os.Args[2])
+	force := flag.Bool("force", false, "Overwrite existing keys")
+	help := flag.Bool("help", false, "Show help text")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: redis-copy [options] <from> <to>\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+	log.Printf("Copying data from: '%s', to: '%s'\n", flag.Args()[0], flag.Args()[1])
+	from := NewClient(flag.Args()[0])
+	to := NewClient(flag.Args()[1])
 	var cursor int64
 	var keys []string
 	var err error
@@ -43,7 +63,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		copyData(keys, from, to)
+		copyData(keys, from, to, force)
 		if cursor == 0 {
 			break
 		}
