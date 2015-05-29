0124c118c552b167a973661da8843c273b97aa2b
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-05-29 11:05:38 +0200

    Added verbose

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ $ ./redis-copy --help
 Usage: redis-copy [options] <from> <to>
   -force=false: Overwrite existing keys
   -help=false: Show help text
+  -verbose=false: Verbose output
 
 # Example
 $ redis-copy localhost:6379 my-redis-in-the-cloud:6379
--- main.go
+++ main.go
@@ -18,7 +18,16 @@ func NewClient(url string) *redis.Client {
 	return client
 }
 
-func copyData(keys []string, from *redis.Client, to *redis.Client, force *bool) {
+var verbose, force *bool
+
+func verboseLog(format string, args ...interface{}) {
+	if *verbose {
+		log.Printf(format, args...)
+	}
+}
+
+func copyData(keys []string, from *redis.Client, to *redis.Client) {
+	verboseLog("Copying %v", keys)
 	for _, key := range keys {
 		dump, err := from.Dump(key).Result()
 		if err != nil {
@@ -39,7 +48,8 @@ func copyData(keys []string, from *redis.Client, to *redis.Client, force *bool) 
 }
 
 func main() {
-	force := flag.Bool("force", false, "Overwrite existing keys")
+	force = flag.Bool("force", false, "Overwrite existing keys")
+	verbose = flag.Bool("verbose", false, "Verbose output")
 	help := flag.Bool("help", false, "Show help text")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: redis-copy [options] <from> <to>\n")
@@ -50,16 +60,23 @@ func main() {
 		flag.Usage()
 		os.Exit(0)
 	}
-	log.Printf("Copying data from: '%s', to: '%s'\n", flag.Args()[0], flag.Args()[1])
+	if len(flag.Args()) < 2 {
+		fmt.Fprintf(os.Stderr, "To few arguments, <from>, <to> are required\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	verboseLog("Copying data from: '%s', to: '%s'\n", flag.Args()[0], flag.Args()[1])
 	from := NewClient(flag.Args()[0])
 	to := NewClient(flag.Args()[1])
 	for {
+		var cursor int64
 		scanCmd := from.Scan(cursor, "*", 100)
 		cursor, keys, err := scanCmd.Result()
 		if err != nil {
 			log.Fatal(err)
 		}
-		copyData(keys, from, to, force)
+		copyData(keys, from, to)
 		if cursor == 0 {
 			break
 		}
